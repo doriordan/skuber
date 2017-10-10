412483f4b325248589982efc1c10f1e6dd621d33
Dave O'Riordan <dave.oriordan@workday.com>
2017-10-10 18:23:14 +0100

    Modified ListQueryParamSpec to not use MockWS

--- client/src/test/scala/skuber/api/ListQueryParamSpec.scala
+++ client/src/test/scala/skuber/api/ListQueryParamSpec.scala
@@ -2,89 +2,29 @@ package skuber.api
 
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
-
 import skuber._
 
 /**
-  * NOTE: This entire test needs a revisit:
-  * - Doesnt succeed on Travis CI now for some unknown reason (it worked previously)
-  * - Skuber release 2.0 moves away from Play WS altogether to Akka HTTP, hence want to revisit use of Mock WS
-  * For the moment, the test and Play dependencies are basically just commented out
-
-import play.api.libs.json.Json
-import play.api.mvc._
-import play.api.mvc.Results._
-import play.api.http.HttpVerbs._
-
-import mockws.MockWS
+  * NOTE: This was MockWS based, but not any more due to:
+  * - unexplained failures suddenly started to happen when running on Travis CI (but not locally)
+  * - skuber 2.0 has migrated away from Play WS client
 */
-
-import scala.concurrent.duration._
 
 import LabelSelector.dsl._
 
 /**
  * @author Chris Baker
+ * @author David O'Riordan
  */
 class ListQueryParamSpec extends Specification {
 
   type EE = ExecutionEnv
 
-  // NOTE: temporarily 'disabled' due to suddenly started faiing on Travis (Issue# 73)
-  "LabelSelector on List is propagated to the API as query parameters" >> { implicit ee: EE =>
+  "LabelSelector string representations equate to the associated labelSelector query param values" >> { implicit ee: EE =>
 
-    true
-    /*
-    val db   = Service("db")
-    val web_blue  = Service("web-blue")
-    val web_green = Service("web-green")
-
-    // i don't want to bother with a bunch of code here to implement selection by labels, so I will just fake the returns
-    // downside: this is fragile because of tight coupling to the calls below, it makes assumptions on the formatting (e.g., whitespace) and on the ordering of the label selectors in the multi-selector case
-    val ws = MockWS {
-      case (GET,"http://server/api/v1/namespaces/default/services") => Action{request =>
-        request.queryString.get("labelSelector") match {
-          case Some(Seq("tier=database"))            => Ok(Json.toJson(ServiceList(items = List(db))))
-          case Some(Seq("tier=web"))                 => Ok(Json.toJson(ServiceList(items = List(web_blue, web_green))))
-          case Some(Seq("tier!=database"))           => Ok(Json.toJson(ServiceList(items = List(web_blue, web_green))))
-          case Some(Seq("tier in (web),stage=blue")) => Ok(Json.toJson(ServiceList(items = List(web_blue))))
-          case None                                  => Ok(Json.toJson(ServiceList(items = List(db,web_blue,web_green))))
-          case _                                     => BadRequest("test not written for this")
-        }
-      }
-    }
-
-    val sk8 = new RequestContext( ws.url(_), "http://server", HTTPRequestAuth.NoAuth, Namespace.default.name, () => ws.close())
-
-    sk8.list[ServiceList]().map(_.items) must containTheSameElementsAs(List(db,web_blue,web_green)).await(5, 5 seconds)
-
-    sk8.list[ServiceList](LabelSelector("tier" is "database")).map(_.items) must containTheSameElementsAs(List(db)).await(5, 5 seconds)
-
-    sk8.list[ServiceList](LabelSelector("tier" is "web")).map(_.items) must containTheSameElementsAs(List(web_blue,web_green)).await(5, 5 seconds)
-
-    sk8.list[ServiceList](LabelSelector("tier" isNot "database")).map(_.items) must containTheSameElementsAs(List(web_blue,web_green)).await(5, 5 seconds)
-
-    sk8.list[ServiceList](LabelSelector("tier" isIn List("web"), "stage" is "blue")).map(_.items) must containTheSameElementsAs(List(web_blue)).await(5, 5 seconds)
-  */
-  }
-
-  // NOTE: temporarily 'disabled' due to suddenly started faiing on Travis (Issue# 73)
-  "No labelSelector query parameter if LabelSelector not specified to ::list" >> { implicit ee: EE =>
-
-    true
-    /*
-    val ws = MockWS {
-      case (GET,"http://server/api/v1/namespaces/default/pods") => Action{request =>
-        request.queryString.get("labelSelector") match {
-          case None => Ok(Json.toJson(PodList(List())))
-          case _ => BadRequest("test not written for this")
-        }
-      }
-    }
-
-    val sk8 = new RequestContext( ws.url(_), "http://server", HTTPRequestAuth.NoAuth, Namespace.default.name, () => ws.close())
-
-    sk8.list[PodList]().map(_.items.isEmpty) must beTrue.await(5, 5 seconds)
-    */
+    LabelSelector("tier" is "database").toString mustEqual "tier=database"
+    LabelSelector("tier" isNot "database").toString mustEqual "tier!=database"
+    LabelSelector("tier" isIn List("web","database")).toString mustEqual "tier in (web,database)"
+    LabelSelector("tier" isIn List("database","app"), "version" is "1").toString mustEqual "tier in (database,app),version=1"
   }
 }
