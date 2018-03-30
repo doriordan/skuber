ace893c70d40bec9cde79f3208f8dc1a2410de4b
Chris Baker <chris@cgbaker.net>
2018-03-30 11:01:01 -0400

    added dependency bouncycastle for parsing private keys

--- client/src/main/scala/skuber/api/security/SecurityHelper.scala
+++ client/src/main/scala/skuber/api/security/SecurityHelper.scala
@@ -1,14 +1,16 @@
 package skuber.api.security
 
-import scala.io.Source
-import java.io.{InputStream, ByteArrayInputStream, FileInputStream}
-import java.nio.file.{Path,Paths,Files}
-import java.util.Base64
-import java.security.{KeyStore, KeyFactory, PrivateKey }
-import java.security.cert.{Certificate, X509Certificate, CertificateFactory}
-import java.security.spec.{PKCS8EncodedKeySpec, RSAPrivateKeySpec}
-import scala.collection.JavaConverters._
+import java.io._
+import java.nio.file.{Files, Paths}
+import java.security.{KeyStore, PrivateKey}
+import java.security.cert.{CertificateFactory, X509Certificate}
 
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
+import org.bouncycastle.openssl.{PEMKeyPair, PEMParser}
+
+import scala.collection.JavaConverters._
 import skuber.api.client.PathOrData
 
 /**
@@ -61,27 +63,21 @@ object SecurityHelper {
    * Note: Currently the stream must contain a PEM format file with a PKCS#8 encoded key
    */
   
-  def readPrivateKey(is: InputStream, keyAlgo: String = "RSA"): PrivateKey = {   
-    val keyPemLines = Source.fromInputStream(is).getLines.toList
-    val isPKCS1 = keyPemLines.headOption.map(_.contains(" RSA ")).getOrElse(false)
-    val keyLines = keyPemLines.filterNot (l => l.contains("-----BEGIN") || l.contains("-----END"))
-    val keyBase64Encoded = keyLines.mkString // concat the key lines to give a single base64 encoded key
-    val keyBytes: Array[Byte] = Base64.getDecoder.decode(keyBase64Encoded) 
-    val keySpec = 
-      if (isPKCS1)
-        SecurityHelper.PCKS1Helper.decodeKey(keyBytes)
-      else {  
-        // if not PKCS#1 then assume it is PKCS#8
-        val keFactory = KeyFactory.getInstance(keyAlgo)   
-        new PKCS8EncodedKeySpec(keyBytes)
+  def readPrivateKey(is: InputStream): PrivateKey = {
+    val provider = new BouncyCastleProvider()
+    val converter = new JcaPEMKeyConverter().setProvider(provider);
+    Option(new PEMParser(new InputStreamReader(is)).readObject()) match {
+      case Some(obj) => obj match {
+        case pair: PEMKeyPair => converter.getPrivateKey(pair.getPrivateKeyInfo)
+        case pk: PrivateKeyInfo => converter.getPrivateKey(pk)
       }
-    val kf = KeyFactory.getInstance("RSA")
-    kf.generatePrivate(keySpec)
+      case None => throw new IOException("could not read private key")
+    }
   }
   
-  def getPrivateKey(from: PathOrData, keyAlgo: String = "RSA") : PrivateKey = {
+  def getPrivateKey(from: PathOrData) : PrivateKey = {
     val is = createInputStreamForPathOrData(from)
-    readPrivateKey(is, keyAlgo)
+    readPrivateKey(is)
   }
   
   /*
@@ -93,83 +89,6 @@ object SecurityHelper {
     keyStore.load(null, keyStorePassword)
     keyStore.setKeyEntry(user, clientPrivateKey, keyStorePassword, clientCertificates.toArray)
     keyStore
-  } 
-  
-  private object PCKS1Helper {
-
-    // This helper supports cases where private key is PKCS1 encoded, as unlike PKCS8 we don't seem to get standard Java
-    // library support for parsing keys encoded in that format
-    
-    // The code below is partly inspired by - and therefore credit owed to authors of - source of net.auth.core Java lib
-    
-    import java.io.IOException
-    import java.math.BigInteger;
-    import java.security.spec.RSAPrivateCrtKeySpec;
-    
-    case class ASN1Value(id: Int, contents: Array[Byte]) {
-      
-      def asBigInteger: BigInteger = {
-        if ((id & 0x1f) != 0x02)
-          throw new IOException("Invalid DER format for private key: expected INTEGER")
-        new BigInteger(contents)
-      }
-      
-    }
-    
-    class DERParser(input: ByteArrayInputStream) {
-      
-      def readASN1: ASN1Value = {
-        val id = input.read
-        val lenInitialByte = input.read
-        val len = if (lenInitialByte <= 0x7f)
-                     lenInitialByte 
-                  else {
-                    // length is encoded over several octets
-                    val numBytesInLenEncoding = lenInitialByte & 0x7f
-                    if (numBytesInLenEncoding < 1 || numBytesInLenEncoding > 4)
-                      throw new IOException("Invalid DER format for private key: bad length encoding")
-                    val lenEncodingBytes= new Array[Byte](numBytesInLenEncoding)
-                    val numRead = input.read(lenEncodingBytes,0,numBytesInLenEncoding)
-                    if (numRead < numBytesInLenEncoding)
-                      throw new IOException("Invalid DER format for private key: not enough bytes for length encoding")
-                    new BigInteger(1, lenEncodingBytes).intValue
-                  } 
-        val content = new Array[Byte](len)
-        val numRead = input.read(content,0,len)
-        if (numRead < len)
-            throw new IOException("Invalid DER format for private key: not enough bytes for content encoding")
-        ASN1Value(id,content)
-      }
-    }
-    def decodeKey(pkcs1Bytes: Array[Byte]) : RSAPrivateKeySpec = {
-      val input=new ByteArrayInputStream(pkcs1Bytes)
-      val seqParser = new DERParser(input)
-      val sequence = seqParser.readASN1
-      if ((sequence.id & 0x3f) != 0x30)      
-        throw new IOException("Invalid DER format for private key: expected SEQUENCE, got: " + sequence.id)
-      
-      val encapsulatedInput=new ByteArrayInputStream(sequence.contents)
-      val parser = new DERParser(encapsulatedInput)
-      parser.readASN1
-      
-      val modulus=parser.readASN1.asBigInteger
-      val publicExponent = parser.readASN1.asBigInteger
-      val privateExponent = parser.readASN1.asBigInteger
-      val primeP = parser.readASN1.asBigInteger
-      val primeQ = parser.readASN1.asBigInteger
-      val primeExponentP = parser.readASN1.asBigInteger
-      val primeExponentQ = parser.readASN1.asBigInteger
-      val crtCoefficient = parser.readASN1.asBigInteger
-      
-      new RSAPrivateCrtKeySpec(
-        modulus,
-        publicExponent,
-        privateExponent,
-        primeP,
-        primeQ,
-        primeExponentP,
-        primeExponentQ,
-        crtCoefficient)
-    }
   }
+
 }
