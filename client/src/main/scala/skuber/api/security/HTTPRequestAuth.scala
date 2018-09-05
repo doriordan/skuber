51dc170e1d49d950511f641ae749b67b2d43dd73
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2018-09-06 02:30:55 +0900

    Support pod exec (#195)
    * Implement pod exec
    
    * Add pod exec tests
    
    * Add pod exec examples
    
    * Set status code of websocket upgrade response
    
    * Assert by status codes instead of messages

--- client/src/main/scala/skuber/api/security/HTTPRequestAuth.scala
+++ client/src/main/scala/skuber/api/security/HTTPRequestAuth.scala
@@ -1,6 +1,6 @@
 package skuber.api.security
 
-import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest}
 import akka.http.scaladsl.model.headers.{Authorization, BasicHttpCredentials, OAuth2BearerToken}
 import skuber.api.client._
 
@@ -10,10 +10,18 @@ import skuber.api.client._
 object HTTPRequestAuth {
   
   def addAuth(request: HttpRequest, auth: AuthInfo) : HttpRequest = {
+    getAuthHeaders(auth).foreach { header =>
+      // Add headers one by one because `addHeaders()` doesn't convert the instance type
+      request.addHeader(header)
+    }
+    request
+  }
+
+  def getAuthHeaders(auth: AuthInfo) : Seq[HttpHeader] = {
     auth match {
-      case NoAuth | _: CertAuth => request
-      case BasicAuth(user, password) => request.addHeader(Authorization(BasicHttpCredentials(user,password)))
-      case auth: AccessTokenAuth => request.addHeader(Authorization(OAuth2BearerToken(auth.accessToken)))
+      case NoAuth | _: CertAuth => Seq()
+      case BasicAuth(user, password) => Seq(Authorization(BasicHttpCredentials(user,password)))
+      case auth: AccessTokenAuth => Seq(Authorization(OAuth2BearerToken(auth.accessToken)))
     }
   }   
 }
