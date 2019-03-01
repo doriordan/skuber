bf54cae27a2906e8c3c249f6949f45d9b3f0e8c6
maasg <gerard.maas@gmail.com>
2019-03-01 16:25:13 +0100

    update PVC definition following the kubernetes specification

--- client/src/main/scala/skuber/PersistentVolumeClaim.scala
+++ client/src/main/scala/skuber/PersistentVolumeClaim.scala
@@ -1,47 +1,58 @@
 package skuber
 
-import java.util.Date
-
-import Volume._
+import skuber.annotation.MatchExpression
 
 /**
- * @author David O'Riordan
- */
+  * @author David O'Riordan
+  *
+  */
+
 case class PersistentVolumeClaim(
-    val kind: String ="PersistentVolumeClaim",
+    val kind: String = "PersistentVolumeClaim",
     override val apiVersion: String = v1,
     val metadata: ObjectMeta = ObjectMeta(),
     spec: Option[PersistentVolumeClaim.Spec] = None,
     status: Option[PersistentVolumeClaim.Status] = None)
-      extends ObjectResource {
-  
-  def withResourceVersion(version: String) = this.copy(metadata = metadata.copy(resourceVersion=version))
+  extends ObjectResource {
+
+  def withResourceVersion(version: String) = this.copy(metadata = metadata.copy(resourceVersion = version))
 
 }
-   
+
+case class Selector(matchLabels: Option[Map[String, String]] = None, matchExpressions: Option[List[MatchExpression]] = None)
+
 object PersistentVolumeClaim {
 
-  val specification=CoreResourceSpecification(
+  object VolumeMode extends Enumeration {
+    type VolumeMode = Value
+    val Filesystem, BlockVolume = Value
+  }
+
+  val specification = CoreResourceSpecification(
     scope = ResourceSpecification.Scope.Namespaced,
     names = ResourceSpecification.Names(
-      plural="persistentvolumeclaims",
-      singular="persistentvolumeclaim",
-      kind="PersistentVolumeClaim",
-      shortNames=List("pvc")
+      plural = "persistentvolumeclaims",
+      singular = "persistentvolumeclaim",
+      kind = "PersistentVolumeClaim",
+      shortNames = List("pvc")
     )
   )
-  implicit val pvcDef = new ResourceDefinition[PersistentVolumeClaim] { def spec=specification }
-  implicit val pvcListDef = new ResourceDefinition[PersistentVolumeClaimList] { def spec=specification }
+  implicit val pvcDef = new ResourceDefinition[PersistentVolumeClaim] { val spec = specification }
+  implicit val pvcListDef = new ResourceDefinition[PersistentVolumeClaimList] { val spec = specification }
 
   import PersistentVolume.AccessMode
   case class Spec(
-      accessModes: List[AccessMode.AccessMode] = Nil,
-      resources: Option[Resource.Requirements] = None,
-      volumeName: String="",
-      storageClassName: Option[String] = None)
-      
-  import PersistentVolume.Phase    
+    accessModes: List[AccessMode.AccessMode] = Nil,
+    volumeMode: VolumeMode.VolumeMode,
+    resources: Option[Resource.Requirements] = None,
+    storageClassName: Option[String] = None,
+    selector: Option[Selector] = None
+  )
+
+  import PersistentVolume.Phase
   case class Status(
-      phase: Option[Phase.Phase] = None,
-      accessModes: List[AccessMode.AccessMode] = List())
+    phase: Option[Phase.Phase] = None,
+    accessModes: List[AccessMode.AccessMode] = List()
+  )
+
 }
