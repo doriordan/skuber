7e28b54359f2ed9ac9b4aa2ddc88347c04358ce8
Iurii Malchenko <imalchenko@arazoo.com>
2020-02-21 05:41:05 +0200

    few tweaks to the Ingress dsl

--- client/src/main/scala/skuber/networking/Ingress.scala
+++ client/src/main/scala/skuber/networking/Ingress.scala
@@ -19,35 +19,85 @@ case class Ingress(
 
   lazy val copySpec: Ingress.Spec = this.spec.getOrElse(new Ingress.Spec)
 
-  /*
+  /**
    * Fluent API method for building out ingress rules e.g.
+   * {{{
    * val ingress = Ingress("microservices").
    *   addHttpRule("foo.bar.com",
-   *                Map("/order" -> "orderService:80", "inventory" -> "inventoryService:80")).
+   *                Map("/order" -> "orderService:80",
+   *                    "inventory" -> "inventoryService:80")).
    *   addHttpRule("foo1.bar.com",
-   *                Map("/ship" -> "orderService:80", "inventory" -> "inventoryService:80")).
-   *
+   *                Map("/ship" -> "orderService:80",
+   *                    "inventory" -> "inventoryService:80")).
+   * }}}
    */
-  def addHttpRule(host:String, pathsMap: Map[String, String]): Ingress =
+  def addHttpRule(host: String, pathsMap: Map[String, String]): Ingress =
     addHttpRule(Some(host), pathsMap)
+
+  /**
+   * Fluent API method for building out ingress rules without host e.g.
+   * {{{
+   * val ingress = Ingress("microservices").
+   *   addHttpRule(Map("/order" -> "orderService:80",
+   *                    "inventory" -> "inventoryService:80")).
+   *   addHttpRule(Map("/ship" -> "orderService:80",
+   *                    "inventory" -> "inventoryService:80")).
+   * }}}
+   */
+  def addHttpRule(pathsMap: Map[String, String]): Ingress =
+    addHttpRule(Option.empty, pathsMap)
+
+  /**
+   * Fluent API method for building out ingress rules e.g.
+   * {{{
+   * val ingress =
+   *   Ingress("microservices")
+   *     .addHttpRule("foo.bar.com",
+   *                  "/order" -> "orderService:80",
+   *                  "inventory" -> "inventoryService:80")
+   *     .addHttpRule("foo1.bar.com",
+   *                  "/ship" -> "orderService:80",
+   *                  "inventory" -> "inventoryService:80").
+   * }}}
+   */
+  def addHttpRule(host: String, pathsMap: (String, String)*): Ingress =
+    addHttpRule(Some(host), pathsMap.toMap)
+
+  /**
+   * Fluent API method for building out ingress rules without host e.g.
+   * {{{
+   * val ingress =
+   *   Ingress("microservices")
+   *     .addHttpRule("/order" -> "orderService:80",
+   *                  "inventory" -> "inventoryService:80")
+   *     .addHttpRule("/ship" -> "orderService:80",
+   *                  "inventory" -> "inventoryService:80").
+   * }}}
+   */
+  def addHttpRule(pathsMap: (String, String)*): Ingress =
+    addHttpRule(Option.empty, pathsMap.toMap)
 
   private val backendSpec = "(\\S+):(\\S+)".r
 
-  /*
+  /**
    * Fluent API method for building out ingress rules e.g.
-   * val ingress = Ingress("microservices").
-   *   addHttpRule(Some("foo.bar.com"),
-   *                Map("/order" -> "orderService:80", "inventory" -> "inventoryService:80")).
-   *   addHttpRule(None,
-   *                Map("/ship" -> "orderService:80", "inventory" -> "inventoryService:80")).
-   *
+   * {{{
+   * val ingress =
+   *   Ingress("microservices")
+   *     .addHttpRule(Some("foo.bar.com"),
+   *                  Map("/order" -> "orderService:80",
+   *                      "inventory" -> "inventoryService:80"))
+   *     .addHttpRule(None,
+   *                  Map("/ship" -> "orderService:80",
+   *                      "inventory" -> "inventoryService:80")).
+   * }}}
    */
   def addHttpRule(host: Option[String], pathsMap: Map[String, String]): Ingress = {
     val paths: List[Ingress.Path] = pathsMap.map { case (path: String, backend: String) =>
       backend match {
         case backendSpec(serviceName, servicePort) =>
           Ingress.Path(path,Ingress.Backend(serviceName, servicePort))
-        case _ => throw new Exception("invalid backend format: expected \"serviceName:servicePort\"")
+        case _ => throw new Exception(s"invalid backend format: expected 'serviceName:servicePort' (got '$backend', for host: $host)")
       }
 
     }.toList
@@ -57,7 +107,25 @@ case class Ingress(
     this.copy(spec = Some(withRuleSpec))
   }
 
-  // set the default backend i.e. if no ingress rule matches the incoming traffic then it gets routed to the specified service
+  /**
+   * set the default backend i.e. if no ingress rule matches the incoming traffic then it gets routed to the specified service
+   * @param serviceNameAndPort - service name and port as 'serviceName:servicePort'
+   * @return copy of this Ingress with default backend set
+   */
+  def withDefaultBackendService(serviceNameAndPort: String): Ingress = {
+    serviceNameAndPort match {
+      case backendSpec(serviceName, servicePort) =>
+        withDefaultBackendService(serviceName, servicePort)
+      case _ => throw new Exception(s"invalid default backend format: expected 'serviceName:servicePort' (got '$serviceNameAndPort')")
+    }
+  }
+
+  /**
+   * set the default backend i.e. if no ingress rule matches the incoming traffic then it gets routed to the specified service
+   * @param serviceName - service name
+   * @param servicePort - service port
+   * @return copy of this Ingress with default backend set
+   */
   def withDefaultBackendService(serviceName: String, servicePort: String): Ingress = {
     val be = Backend(serviceName, servicePort)
     this.copy(spec=Some(copySpec.copy(backend = Some(be)))
