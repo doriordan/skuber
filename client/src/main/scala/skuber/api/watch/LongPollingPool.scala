773cb6cede47d6fed496f8cd321cd560a78a53b8
Arnout Engelen <arnout@bzzt.net>
2020-09-09 15:33:26 +0200

    Don't use a 1-connection pool at all, but directly create a connection

--- client/src/main/scala/skuber/api/watch/LongPollingPool.scala
+++ client/src/main/scala/skuber/api/watch/LongPollingPool.scala
@@ -4,8 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.settings.{ClientConnectionSettings, ConnectionPoolSettings}
 import akka.http.scaladsl.{Http, HttpsConnectionContext}
-import akka.stream.Materializer
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import skuber.api.client.Pool
 
 import scala.concurrent.duration._
@@ -18,29 +17,26 @@ private[api] object LongPollingPool {
                clientConnectionSettings: ClientConnectionSettings)(implicit system: ActorSystem): Pool[T] = {
     implicit val ec = system.dispatcher
     schema match {
-      case "http" | "https" =>
+      case "http" =>
         Flow[(HttpRequest, T)]
           .mapAsync(1) {
             case (request, userdata) =>
-              Http()
-                .singleRequest(
-                  request,
-                  httpsConnectionContext.getOrElse(Http().defaultClientHttpsContext),
-                  buildHostConnectionPool(poolIdleTimeout, clientConnectionSettings, system)
-                )
+              Source.single(request)
+                .via(Http().outgoingConnection(host, port, settings = clientConnectionSettings))
                 .map(response => (Success(response), userdata))
+                .runWith(Sink.head)
+          }
+      case "https" =>
+        Flow[(HttpRequest, T)]
+          .mapAsync(1) {
+            case (request, userdata) =>
+              Source.single(request)
+                .via(Http().outgoingConnectionHttps(host, port, httpsConnectionContext.get, settings = clientConnectionSettings))
+                .map(response => (Success(response), userdata))
+                .runWith(Sink.head)
           }
       case unsupported =>
         throw new IllegalArgumentException(s"Schema $unsupported is not supported")
     }
-  }
-
-  private def buildHostConnectionPool[T](poolIdleTimeout: Duration, clientConnectionSettings: ClientConnectionSettings, system: ActorSystem) = {
-    ConnectionPoolSettings(system)
-      .withMaxConnections(1)              // Limit number the of open connections to one
-      .withPipeliningLimit(1)             // Limit pipelining of requests to one
-      .withMaxRetries(0)                  // Disables retries
-      .withIdleTimeout(poolIdleTimeout)   // Automatically shutdown connection pool after timeout
-      .withConnectionSettings(clientConnectionSettings)
   }
 }
