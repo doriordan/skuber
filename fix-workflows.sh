1b2497d848128adcd46ce0bac1638644b54f4cb0
hagay3 <hagay3@gmail.com>
2022-05-25 23:48:19 +0300

    fix ci running twice

--- fix-workflows.sh
+++ fix-workflows.sh
@@ -7,3 +7,5 @@ sed -i ''  -E 's/.*githubWorkflowCheck//' .github/workflows/ci.yml
 sed -i ''  -E 's/minikubeversion/minikube version/' .github/workflows/ci.yml
 sed -i ''  -E 's/kubernetesversion/kubernetes version/' .github/workflows/ci.yml
 sed -i ''  -E 's/githubtoken/github token/' .github/workflows/ci.yml
+sed -i ''  -E '1,/branches.*/s/branches.*//' .github/workflows/ci.yml
+sed -i ''  -E 's/pull_request://' .github/workflows/ci.yml
