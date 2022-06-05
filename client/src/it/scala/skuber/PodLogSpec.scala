d870e27c516ca7e6d52d9f74caf7ffe7212bd2f0
hagay3 <hagay3@gmail.com>
2022-06-05 10:12:38 +0300

    improve PodLog stability

--- client/src/it/scala/skuber/PodLogSpec.scala
+++ client/src/it/scala/skuber/PodLogSpec.scala
@@ -1,47 +1,39 @@
 package skuber
 
+import java.util.UUID.randomUUID
 import com.typesafe.config.ConfigFactory
-import org.scalatest.concurrent.Eventually
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.{BeforeAndAfterAll, Matchers}
 import skuber.Pod.LogQueryParams
 import skuber.json.format._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class PodLogSpec extends K8SFixture with Eventually with Matchers with BeforeAndAfterAll {
-  val podName: String = java.util.UUID.randomUUID().toString
+class PodLogSpec extends K8SFixture with Eventually with Matchers with BeforeAndAfterAll with ScalaFutures {
+  val podName: String = randomUUID().toString
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(10.second)
 
   behavior of "PodLog"
 
-  val idleTimeout = 3.seconds
+  private val idleTimeout = 10.seconds
   override val config = ConfigFactory.parseString(s"skuber.pod-log.idle-timeout=${idleTimeout.toSeconds}s").withFallback(ConfigFactory.load())
 
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-
-    val k8s = k8sInit(config)
-    Await.result(k8s.create(getNginxPod(podName, "1.7.9")), 3.second)
-    // Let the pod running
-    Thread.sleep(15000)
-    k8s.close
-  }
 
   override def afterAll(): Unit = {
     val k8s = k8sInit(config)
-    Await.result(k8s.delete[Pod](podName), 3.second)
-    Thread.sleep(3000)
-    k8s.close
-
-    super.afterAll()
+    val result = k8s.delete[Pod](podName).recover{ case _ => () }
+    result.futureValue
+    result.onComplete(_ => k8s.close )
   }
 
   it should "get log of a pod" in { k8s =>
+    k8s.create(getNginxPod(podName, "1.7.9"))
+    Thread.sleep(3000)
     eventually(timeout(30.seconds), interval(3.seconds)) {
-      k8s.getPodLogSource(podName, LogQueryParams(follow = Some(false))).flatMap { source =>
-        source.map(_.utf8String).runReduce(_ + _).map { s =>
-          assert(s == "foo\n")
-        }
-      }
+      val source = k8s.getPodLogSource(podName, LogQueryParams(follow = Some(false))).futureValue
+      val log = source.map(_.utf8String).runReduce(_ + _).futureValue
+      assert(log == "foo\n")
     }
   }
 
@@ -54,6 +46,6 @@ class PodLogSpec extends K8SFixture with Eventually with Matchers with BeforeAnd
   def getNginxPod(name: String, version: String): Pod = {
     val container = getNginxContainer(version)
     val podSpec = Pod.Spec(containers = List((container)))
-    Pod.named(podName).copy(spec = Some(podSpec))
+    Pod.named(name).copy(spec = Some(podSpec))
   }
 }
