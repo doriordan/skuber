849f59de20664395a6132c7e8fb6df4da87b0691
hagay3 <hagay3@gmail.com>
2022-06-05 21:15:30 +0300

    add akka terminate

--- client/src/it/scala/skuber/K8SFixture.scala
+++ client/src/it/scala/skuber/K8SFixture.scala
@@ -1,10 +1,8 @@
 package skuber
 
 import akka.actor.ActorSystem
-import org.scalatest.{FutureOutcome, fixture}
-import skuber.api.client._
 import com.typesafe.config.ConfigFactory
-import skuber.api.client.impl.KubernetesClientImpl
+import org.scalatest.{FutureOutcome, fixture}
 
 trait K8SFixture extends fixture.AsyncFlatSpec {
 
@@ -21,6 +19,7 @@ trait K8SFixture extends fixture.AsyncFlatSpec {
       withFixture(test.toNoArgAsyncTest(k8s))
     } lastly {
       k8s.close
+      system.terminate()
     }
   }
 }
