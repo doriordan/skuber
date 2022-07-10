63b6f644f179ca678caa8ab2963d61b2097216dd
hagay3 <hagay3@gmail.com>
2022-07-10 21:16:11 +0300

    run ci on pull requests

--- infra/ci/fix-workflows.sh
+++ infra/ci/fix-workflows.sh
@@ -9,5 +9,4 @@ sed -i ''  -E 's/kubernetesversion/kubernetes version/' .github/workflows/ci.yml
 sed -i ''  -E 's/githubtoken/github token/' .github/workflows/ci.yml
 sed -i ''  -E 's/startargs/start args/' .github/workflows/ci.yml
 sed -i ''  -E '1,/branches.*/s/branches.*//' .github/workflows/ci.yml
-sed -i ''  -E 's/pull_request://' .github/workflows/ci.yml
 sed -i ''  -E "s/''/\"/g" .github/workflows/ci.yml
