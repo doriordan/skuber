1a58e34a781007184010514e517f53b5192637c3
Hagai Ovadia <hagay3@gmail.com>
2023-06-23 15:50:57 +0300

    sbt githbu actions upgrade

--- infra/ci/fix-workflows.sh
+++ infra/ci/fix-workflows.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Until the following issue is been fixed, this bash file will fix the ci pipeline.
-# https://github.com/djspiewak/sbt-github-actions/issues/108
 
 sed -i ''  -E 's/.*Check that workflows are up to date//' .github/workflows/ci.yml
 sed -i ''  -E 's/.*githubWorkflowCheck//' .github/workflows/ci.yml
