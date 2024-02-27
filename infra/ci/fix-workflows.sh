d10cc0e773bb373dd777f1addd9d049bf1b6f90d
Hagai Hillel <hagay3@gmail.com>
2024-02-28 01:25:28 +0200

    fix automation

--- infra/ci/fix-workflows.sh
+++ infra/ci/fix-workflows.sh
@@ -4,3 +4,4 @@ sed -i ''  -E 's/.*Check that workflows are up to date//' .github/workflows/ci.y
 sed -i ''  -E 's/.*githubWorkflowCheck//' .github/workflows/ci.yml
 sed -i ''  -E '1,/branches.*/s/branches.*//' .github/workflows/ci.yml
 sed -i ''  -E "s/''/\"/g" .github/workflows/ci.yml
+sed -i ''  '35d' .github/workflows/ci.yml
