75d0d92bfcd40f1e3a9267016240dbc62e209522
Hagai Hillel <hagay3@gmail.com>
2024-02-27 23:58:43 +0200

    push force

--- infra/ci/git-commit-and-push.sh
+++ infra/ci/git-commit-and-push.sh
@@ -10,7 +10,6 @@ git config --global user.email "hagay3@gmail.com"
 git config --global user.name "Hagai Hillel"
 git add version.sbt
 git commit -m "new version"
-git push --force
 
 
 # Define base branch (replace with appropriate branch name)
