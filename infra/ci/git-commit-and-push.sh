1aa8bc7a9330540c0fc79d0edd721b0da422d5d2
Hagai Hillel <hagay3@gmail.com>
2024-02-27 22:49:31 +0200

    push force

--- infra/ci/git-commit-and-push.sh
+++ infra/ci/git-commit-and-push.sh
@@ -5,4 +5,4 @@ git config --global user.email "hagay3@gmail.com"
 git config --global user.name "Hagai Hillel"
 git add .
 git commit -m "new version"
-git push origin master
+git push origin master --force
