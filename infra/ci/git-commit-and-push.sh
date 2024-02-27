2219959ee922c1bc40b9dc6134bc0a5fb160035a
Hagai Hillel <hagay3@gmail.com>
2024-02-28 00:34:07 +0200

    push force

--- infra/ci/git-commit-and-push.sh
+++ infra/ci/git-commit-and-push.sh
@@ -27,6 +27,7 @@ token=$GITHUB_TOKEN
 
 # Prepare request body for creation
 payload_create="{\"title\": \"$title\", \"body\": \"$body\", \"head\": \"$branch_name\", \"base\": \"$base_branch\"}"
+echo $payload_create
 # Send API request to create pull request
 PULL_NUMBER=$(jq '.number' <<< $(curl -s -X POST -H "Authorization: Bearer $token" -d $payload_create "$url_create"))
 
